7093c49474cf112cc54a8b58dc8fad1221daede9
Fabián Lizama <fabian.lizama@usach.cl>
2022-08-23 21:57:55 -0400

    Se agregan funciones bitmap?  pixmap? y hexmap?

--- main.rkt
+++ main.rkt
@@ -14,29 +14,43 @@
 (define-struct pixhex-d (x y hex depth))
 
 ; TDA picture corresponde a una imagen con su largo ancho y lista de pixeles
-(define-struct picture (width length pixels))
+(define-struct picture (width height pixels))
 
-;funciones
+; Funciones
 
+; Función de selección x en común para pixbit-d pixrgb-d y pixhex-d
 (define (get-x pix)
   (cond ((pixbit-d? pix) (pixbit-d-x pix))
         ((pixrgb-d? pix) (pixrgb-d-x pix))
         ((pixhex-d? pix) (pixhex-d-x pix))))
 
+; Función de selección y  en común para pixbit-d pixrgb-d y pixhex-d
 (define (get-y pix)
   (cond ((pixbit-d? pix) (pixbit-d-y pix))
         ((pixrgb-d? pix) (pixrgb-d-y pix))
         ((pixhex-d? pix) (pixhex-d-y pix))))
 
+; Funciones principales
+; TDA image - constructor
 (define (image x y . l)
   (make-picture x y l))
 
+; TDA image - bitmap?
+(define (bitmap? pic)
+  (pixbit-d? (car(picture-pixels pic))))
+
+; TDA image - pixmap?
+(define (pixmap? pic)
+  (pixrgb-d? (car(picture-pixels pic))))
+
+; TDA image - hexmap?
+(define (hexmap? pic)
+  (pixhex-d? (car(picture-pixels pic))))
+
+
+
 ; ejemplos
 (define pix (pixbit-d 4 3 2 1))
-(define ejimage (image 2 2 (pixbit-d  0 0 1 10) (pixbit-d  0 1 0 20) (pixbit-d 1 0 0 30) (pixbit-d 1 1 1 4)))
-
-;(define example (picture 2 2 (list pix pix)))
-
-;(pixbit-d-x (car (picture-pixels example)))
-
-;(image 2 2 (list pix pix))
+(define ejimageb (image 2 2 (pixbit-d  0 0 1 10) (pixbit-d  0 1 0 20) (pixbit-d 1 0 0 30) (pixbit-d 1 1 1 4)))
+(define ejimager (image 1 1 (pixrgb-d 1 1 1 1 1 1)))
+(define ejimageh (image 1 1 (pixhex-d 1 1 "#FF0000" 1)))
