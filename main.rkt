d8c8290c1d40d15f686a4342dfdf70f554eab035
Fabián Lizama <fabian.lizama@usach.cl>
2022-09-22 01:53:43 -0300

    Se implementa función depthLayers

--- main.rkt
+++ main.rkt
@@ -27,7 +27,7 @@
 
 ; Funciones
 
-(define setR (lambda (r pix) (pixrgb-d (get-x pix) (get-y pix) r (get-g pix) (get-b pix) (get-d pix))))
+(define setR (lambda (r pix) (pixrgb-d (get-x pix) (get-y pix) r (getG pix) (getB pix) (get-d pix))))
 
 (define (get-bit pix) (list-ref pix 3))
 
@@ -66,9 +66,10 @@
 (define (get-hex pix)
   (list-ref pix 3))
 
-(define (get-r pix) (list-ref pix 3))
-(define (get-g pix) (list-ref pix 4))
-(define (get-b pix) (list-ref pix 5))
+(define (getR pix) (list-ref pix 3))
+(define (getG pix) (list-ref pix 4))
+(define (getB pix) (list-ref pix 5))
+(define getColor car)
 
 
 ; Funciones principales
@@ -97,7 +98,7 @@
 ; TDA image - bitmap?
 ; Función que permite determinar si la imagen corresponde a un bitmap-d
 ; Dominio: image Recorrido: boolean
-(define (bitmap? pic) (pixbit-d? (car (list-ref pic 3))))
+(define (bitmap? pic) (pixbit-d? (car (get-pixlist pic))))
 
 ; TDA image - pixmap?
 ; Función que permite determinar si la imagen corresponde a un pixmap-d
@@ -126,12 +127,12 @@
 ; Esta función modifica el valor y de un pixel
 ; Dominio: y (int) X [pixbit-d | pixrgb-d | pixhex-d]
 ; Recorrido: [pixbit-d | pixrgb-d | pixhex-d]
-(define (mod-y y pix)
+(define (setY y pix)
   (cond ((= 0 (car pix)) (pixbit-d (get-x pix) y (list-ref pix 3) (list-ref pix 4)))
         ((= 1 (car pix)) (pixrgb-d (get-x pix) y (list-ref pix 3) (list-ref pix 4) (list-ref pix 5) (list-ref pix 6)))
         ((= 2 (car pix)) (pixhex-d (get-x pix) y (list-ref pix 3) (list-ref pix 4)))))
 
-(define (mod-x x pix)
+(define (setX x pix)
   (cond ((= 0 (car pix)) (pixbit-d x (get-y pix) (list-ref pix 3) (list-ref pix 4)))
         ((= 1 (car pix)) (pixrgb-d x (get-y pix) (list-ref pix 3) (list-ref pix 4) (list-ref pix 5) (list-ref pix 6)))
         ((= 2 (car pix)) (pixhex-d x (get-y pix) (list-ref pix 3) (list-ref pix 4)))))
@@ -148,7 +149,7 @@
 ; Dominio: pix ([pixbit-d | pixrgb-d | pixhex-d]) X width (int)
 ; Recorrido: pix ([pixbit-d | pixrgb-d | pixhex-d])
 (define (flipH-pix pix w)
-  (mod-x (abs (- (get-x pix) (- w 1))) pix))
+  (setX (abs (- (get-x pix) (- w 1))) pix))
 
 
 
@@ -164,14 +165,14 @@
 ; Dominio: pix ([pixbit-d | pixrgb-d | pixhex-d]) X width (int)
 ; Recorrido: pix ([pixbit-d | pixrgb-d | pixhex-d])
 (define (flipV-pix pix w)
-  (mod-y (abs (- (get-y pix) (- w 1))) pix))
+  (setY (abs (- (get-y pix) (- w 1))) pix))
 
 
 ; TDA image - crop: Recortar una imagen a partir de un cuadrante
 ; Dominio: image X x1 (int) X y1 (int) X x2 (int) X y2 (int)
 ; Recorrido: image
 (define (crop pic x1 y1 x2 y2)
-  (image-list (if (compressed? pic) (car pic) -1) (+ (- x2 x1) 1) (+ (- y2 y1) 1) (map (lambda (pix) (mod-y (- (get-y pix) y1) (mod-x (- (get-x pix) x1) pix))) (myfilter condi (get-pixlist pic) x1 y1 x2 y2))))
+  (image-list (if (compressed? pic) (car pic) -1) (+ (- x2 x1) 1) (+ (- y2 y1) 1) (map (lambda (pix) (setY (- (get-y pix) y1) (setX (- (get-x pix) x1) pix))) (myfilter condi (get-pixlist pic) x1 y1 x2 y2))))
 
 (define (myfilter condition list x1 y1 x2 y2)
   (if (null? list) null
@@ -189,7 +190,7 @@
   (image (get-w pic) (get-h pic) (map pixrgb->pixhex (get-pixlist pic))))
 
 (define (pixrgb->pixhex pix)
-  (list 2 (get-x pix) (get-y pix) (string-append "#" (int->hex (get-r pix)) (int->hex (get-g pix)) (int->hex (get-b pix))) (get-d pix)))
+  (list 2 (get-x pix) (get-y pix) (string-append "#" (int->hex (getR pix)) (int->hex (getG pix)) (int->hex (getB pix))) (get-d pix)))
 
 (define (int->hex x)
   (string-append
@@ -207,6 +208,7 @@
          ((= 13 (remainder x 16)) "D")
          ((= 14 (remainder x 16)) "E")
          ((= 15 (remainder x 16)) "F"))))
+
 
 ; Esta función obtiene el código hexadecimal de cualquier pixel
 (define (pix->hex pix)
@@ -274,15 +276,15 @@
 
 
 ; Función que suma 1 a los pixeles para recuperar las coordenadas originales después de aplicar el crop
-(define pixsum1 (lambda (pix) (mod-x (+ 1 (get-x pix)) (mod-y (+ 1 (get-y pix)) pix))))
+(define pixsum1 (lambda (pix) (setX (+ 1 (get-x pix)) (setY (+ 1 (get-y pix)) pix))))
 
 ; Función que rota el pixel en 90 grados dependiendo del ancho y largo de la imagen
 (define pix90 (lambda (w h)
                 (lambda (pix)
-                  (cond ((= (get-x pix) 0) (mod-x (- (- h 1) (get-y pix)) (mod-y 0 pix)))
-                        ((= (get-x pix) (- w 1)) (mod-x (- (- h 1) (get-y pix)) (mod-y (- w 1) pix)))
-                        ((= (get-y pix) 0) (mod-x (- h 1) (mod-y (get-x pix) pix)))
-                        ((= (get-y pix) (- h 1)) (mod-x 0 (mod-y (get-x pix) pix)))))))
+                  (cond ((= (get-x pix) 0) (setX (- (- h 1) (get-y pix)) (setY 0 pix)))
+                        ((= (get-x pix) (- w 1)) (setX (- (- h 1) (get-y pix)) (setY (- w 1) pix)))
+                        ((= (get-y pix) 0) (setX (- h 1) (setY (get-x pix) pix)))
+                        ((= (get-y pix) (- h 1)) (setX 0 (setY (get-x pix) pix)))))))
 
 ; Función que comprueba si un pixel es parte del anillo exterior de la imagen
 (define ring? (lambda (w h)
@@ -347,9 +349,9 @@
 
 (define invertColorRGB (lambda (pix)
                          (pixrgb-d (get-x pix) (get-y pix)
-                                   (- 255 (get-r pix))
-                                   (- 255 (get-g pix))
-                                   (- 255 (get-b pix))
+                                   (- 255 (getR pix))
+                                   (- 255 (getG pix))
+                                   (- 255 (getB pix))
                                    (get-d pix))))
 
 ; TDA image - adjustChannel
@@ -367,32 +369,95 @@
 (define incCh (lambda (color)
                        (if (< color 255) (+ color 1) color)))
 
+
+; Función redCh recude un canal rgb en uno
+; Dominio: int    Recorrido: int
+(define redCh (lambda (color)
+                       (if (> color 0) (- color 1) color)))
+
+
 ; TDA image - image->string
 ; Función que transforma una imagen a una representación string.
 ; La transformación depende de si la imagen es bitmap-d, hexmap-d o pixmap-d, para lo cual se pasa la función de transformación
 
 (define image->string (lambda (pic f)
+                        ; Función string-recur es una función recursiva de cola que añade a un string vacío los pixeles en orden
+                        ; Dominio: image X string X int X int X int X int        Recorrido: string
                         (define string-recur (lambda
                                                  (pic str x y w h)
-                                                 (string-append str (f (filter ))))
-                        (string-append )))
+                                                       ; Si ya se recorrió toda la imagen se entrega el string resultante
+                                                 (cond ((= y h) str)
+                                                       ; Si ya se recorrió una fila se agrega el salto de línea, se reinicia la coordenada "x", y finalmente se aumenta la "y"
+                                                       ((= x w) (string-recur pic (string-append str "\n") 0 (+ y 1) w h))
+                                                       ; Si no se realiza la recursión de cola aumentando la coordenada "x" y manteniendo la "y"
+                                                       (else (string-recur pic
+                                                                   ; El string de la recursión filtra los pixeles que tengan la coordenada "x" e "y" actuales y concatena con el string acumulado
+                                                                   (if (null? (filter ((lambda (x y w h)
+                                                                                                    (lambda (pix)
+                                                                                                      (if (and (= (get-x pix) x) (= (get-y pix) y)) #t #f))) x y w h) (get-pixlist pic)))
+                                                                       str
+                                                                       (string-append str (f (car (filter ((lambda (x y w h)
+                                                                                                    (lambda (pix)
+                                                                                                      (if (and (= (get-x pix) x) (= (get-y pix) y)) #t #f))) x y w h) (get-pixlist pic)))) "\t"))
+                                                                   (+ x 1) y w h)))))
+                        ; La llamada recursiva inicial parte con un string vacío y en las coordenadas (0, 0)
+                        (string-recur pic "" 0 0 (get-w pic) (get-h pic))))
 
-; Función pixbit-d->string
+; Función pixbit->string
+; Transforma un pixbit-d a string
+; Dominio: pixbit-d        Recorrido: string
 (define pixbit->string (lambda (pix) (number->string (get-bit pix))))
 
-; Función pixrgb-d->string
+; Función pixrgb->string
+; Transforma un pixrgb-d a string
+; Dominio: pixrgb-d        Recorrido: string
 (define pixrgb->string (lambda (pix) (get-hex (pixrgb->pixhex pix))))
 
 ; Función pixhex->string
+; Transforma un pixhex-d a string
+; Dominio: pixhex-d        Recorrido: string
 (define pixhex->string (lambda (pix) (get-hex pix)))
+
+
+; TDA image - depthLayers
+; Función que permite separar una imagen en capas en base a la profundidad en que se sitúan los pixeles.
+; El resultado consiste en una lista de imagenes donde cada una agrupa los pixeles que se sitúan en el mismo nivel de profuncidad.
+; Además, en las imágenes resultantes se sustituyen los pixeles que se encuentran en otro nivel de profundidad por pixeles blancos.
+(define depthLayers (lambda (pic)
+                      (define crop-layers (lambda (pic-list d pixels)
+                                            (cond ((null? pixels) pic-list)
+                                                  ((null? (filter (lambda (pix) (if (= d (get-d pix)) #t #f)) pixels)) (crop-layers pic-list (+ d 1) pixels))
+                                                  (else (crop-layers (cons (white-fill (image-list -1 (get-w pic) (get-h pic) (filter (lambda (pix) (if (= d (get-d pix)) #t #f)) pixels))) pic-list) (+ d 1) (remove* (filter (lambda (pix) (if (= d (get-d pix)) #t #f)) pixels) pixels))))))
+                      (crop-layers '() 0 (get-pixlist pic))))
+
+
+(define white-fill (lambda (pic)
+                     (define recur-fill (lambda (pic x y w h)
+                                         (cond ((= y h) pic)
+                                               ((= x w) (recur-fill pic 0 (+ y 1) w h))
+                                               ((not (null? (filter (lambda (pix) (if (and (= (get-x pix) x) (= (get-y pix) y)) #t #f)) (get-pixlist pic)))) (recur-fill pic (+ x 1) y w h))
+                                               (else (recur-fill (image-list -1 w h (cons (cond ((bitmap? pic) (pixbit-d x y 1 (get-d (car (get-pixlist pic)))))
+                                                                                                ((pixmap? pic) (pixrgb-d x y 255 255 255 (get-d (car (get-pixlist pic)))))
+                                                                                                ((hexmap? pic) (pixhex-d x y "#FFFFFF" (get-d (car (get-pixlist pic))))))
+                                                                                                 (get-pixlist pic))) (+ x 1) y w h)))))
+                     (recur-fill pic 0 0 (get-w pic) (get-h pic))))
+
+
+
+
+
+
 
 ; ejemplos
 (define pix (pixbit-d 4 3 1 1))
 (define pixh (pixhex-d 0 0 "#FFFFFF" 1))
 (define ejimageb (image 2 2 (pixbit-d  0 0 1 10) (pixbit-d  0 1 0 20) (pixbit-d 1 0 0 30) (pixbit-d 1 1 0 30)))
 (define ejimager (image 3 3
-                        (pixrgb-d 0 0 255 0 0 0) (pixrgb-d 0 1 255 0 0 0) (pixrgb-d 0 2 0 0 255 0)
-                        (pixrgb-d 1 0 255 0 0 1) (pixrgb-d 1 1 255 0 0 1) (pixrgb-d 1 2 0 0 255 1)
-                        (pixrgb-d 2 0 255 0 0 2) (pixrgb-d 2 1 255 0 0 2) (pixrgb-d 2 2 0 0 255 2)))
+                        (pixrgb-d 0 0 1 0 0 0) (pixrgb-d 1 0 2 0 0 0) (pixrgb-d 2 0 3 0 0 0)
+                        (pixrgb-d 0 1 4 0 0 2) (pixrgb-d 1 1 5 0 0 2) (pixrgb-d 2 1 6 0 0 2)
+                        (pixrgb-d 0 2 7 0 0 3) (pixrgb-d 1 2 8 0 0 3) (pixrgb-d 2 2 9 0 0 3)))
 
-(define ejimageh (image 1 1 (pixhex-d 1 1 "#FF0000" 1)))
+(define ejimageh (image 4 4 (pixhex-d 0 0 "#FF0000" 1) (pixhex-d 1 0 "#FF0000" 1) (pixhex-d 2 0 "#FF0000" 1) (pixhex-d 3 0 "#FF0000" 1)
+                            (pixhex-d 0 1 "#FF0000" 1) (pixhex-d 1 1 "#FF0000" 1) (pixhex-d 2 1 "#FF0000" 1) (pixhex-d 3 1 "#FF0000" 1)
+                            (pixhex-d 0 2 "#FF0000" 1) (pixhex-d 1 2 "#FF0000" 1) (pixhex-d 2 2 "#FF0000" 1) (pixhex-d 3 2 "#FF0000" 1)
+                            (pixhex-d 0 3 "#FF0000" 1) (pixhex-d 1 3 "#FF0000" 1) (pixhex-d 2 3 "#FF0000" 1) (pixhex-d 3 3 "#FF0000" 1)))
